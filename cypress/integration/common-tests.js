98c91a95ebc3272c2af3ff8b97c4cf36c42e1663
Sairama B <sairama.bonala@wavemaker.com>
2020-01-30 16:34:13 +0530

    Changed locators

--- cypress/integration/common-tests.js
+++ cypress/integration/common-tests.js
@@ -3,9 +3,9 @@ describe('Common tests', () => {
 		cy.signin();
 	});
 
-	it('Check anchors', () => {
+	it('Check anchors in left nav', () => {
 		let anchors = [];
-		cy.get('a')
+		cy.get("app-partial-leftnav > * .app-anchor")
 			.not('.active, a[caption="LOGOUT"], a[caption="Logout"]')
 			.each($el => {
 				if ($el.attr('name')) anchors.push($el.attr('name'));
